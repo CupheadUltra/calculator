c5b6002d8ff789e012455d9ed04754237e25dd76
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2023-08-16 20:05:15 +0300

    calculator

---
+++ calc.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Ділення на нуль неможливе"
+
+print("Оберіть операцію:")
+print("1. Додавання")
+print("2. Віднімання")
+print("3. Множення")
+print("4. Ділення")
+
+choice = input("Введіть номер операції (1/2/3/4): ")
+
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Невірний вибір")
